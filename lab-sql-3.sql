c8bf8a5c98267dbbb88616c7ad7c851465cc7d13
Kirstin Gosling <kirst_gosling@yahoo.com>
2023-02-23 17:46:44 +0100

    submitted lab

--- lab-sql-3.sql
+++ lab-sql-3.sql
@@ -1,11 +1,11 @@
 USE sakila;
 
 # 1. How many distinct (different) actors' last names are there?
-SELECT COUNT(distinct last_name)
+SELECT COUNT(distinct last_name) #count unique last names from 'actor', answer is 121
 FROM actor;
 
 # 2. In how many different languages where the films originally produced? (Use the column language_id from the film table)
-SELECT COUNT(distinct language_id)
+SELECT COUNT(distinct language_id) #the films were produced in 1 language
 FROM film;
 
 # 3. How many movies were released with "PG-13" rating?
@@ -14,13 +14,9 @@ FROM film;
 
 SELECT COUNT(rating)
 FROM film
-WHERE rating LIKE "PG-13";
+WHERE rating LIKE "PG-13"; #there are 223 films with rating PG-13
 
 # 4. Get 10 the longest movies from 2006.
-SELECT COUNT(release_year), title
-FROM film
-WHERE release_year LIKE "2006";
-
 SELECT *
 FROM film
 WHERE release_year LIKE "2006"
@@ -30,17 +26,26 @@ LIMIT 10;
 
 # 5. How many days has been the company operating (check DATEDIFF() function)?
 SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) AS operating_days
-FROM rental;
+FROM rental; # using the first rental date vs the last rental date, the number of operating days is 266
 
 # 6. Show rental info with additional columns month and weekday. Get 20.
 SELECT *
 FROM rental;
 
-SELECT *, substring(rental_date, 6, 2) AS month, DAYNAME(rental
-
-_date, GETDATE()) AS weekday
+SELECT *, substring(rental_date, 6, 2) AS month, 
+CASE
+	WHEN (DAYNAME(rental_date) = "Monday") THEN "Monday"
+    WHEN (DAYNAME(rental_date) = "Tuesday") THEN "Tuesday"
+    WHEN (DAYNAME(rental_date) = "Wednesday") THEN "Wednesday"
+    WHEN (DAYNAME(rental_date) = "Thursday") THEN "Thursday"
+    WHEN (DAYNAME(rental_date) = "Friday") THEN "Friday"
+    WHEN (DAYNAME(rental_date) = "Saturday") THEN "Saturday"
+    ELSE "Sunday"
+    END AS weekday
 FROM rental
 LIMIT 20;
+
+### this feels like a super overcomplicated way to do this, it worked though
 
 # 7. Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
 SELECT  *,
@@ -59,10 +64,4 @@ SELECT rental_id, DATE_FORMAT(rental_date, "%Y-%m-%d") as date_2, datediff("2006
 FROM rental
 WHERE datediff("2006-02-14", DATE_FORMAT(rental_date,"%Y-%m-%d")) < 30;
 
-SELECT * 
-FROM trans
-ORDER BY date DESC;
-
-SELECT trans_id, DATE_FORMAT(date,"%Y-%m-%d") as date_2, datediff("1998-12-31",DATE_FORMAT(date,"%Y-%m-%d")) as date_3
-FROM trans
-WHERE datediff("1998-12-31", DATE_FORMAT(date,"%Y-%m-%d")) < 30;
+### date_3 column only has '0', not sure what I missed here
